0e7ab6c12deff4f8544571afd72941799c93dd4d
Mohamed-Yousry-Ali <mohamedyousry631@gmail.com>
2024-07-31 23:43:40 +0300

    add daynamic form input

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import ProductCard from "./components/ProductCard";
-import { ProductList } from "./data";
+import { formInputList, ProductList } from "./data";
 import Modal from "./components/UI/Modal";
 import Button from "./components/UI/Button";
+import Input from "./components/UI/Input";
 
 const App = () => {
   /* State */
@@ -21,6 +22,18 @@ const App = () => {
   const renderProductList = ProductList.map((product) => (
     <ProductCard key={product.id} product={product} />
   ));
+
+  const renderInputForm = formInputList.map((input) => (
+    <div className="flex flex-col">
+      <label
+        htmlFor={input.id}
+        className="mb-[2px] text-sm font-medium text-gray-700"
+      >
+        {input.label}
+      </label>
+      <Input type="text" id={input.id} name={input.name} />
+    </div>
+  ));
   return (
     <main className="container">
       <Button className="bg-indigo-700 hover:bg-indigo-800" onClick={open}>
@@ -31,9 +44,15 @@ const App = () => {
       </div>
 
       <Modal isOpen={isOpen} close={close} title="Add A New Product">
-        <div className="flex items-center space-x-3">
-          <Button className="bg-indigo-700 hover:bg-indigo-800">Submit</Button>
-          <Button className="bg-gray-700 hover:bg-gray-800">Cancel</Button>
+        <div className="space-y-3">
+          {renderInputForm}
+
+          <div className="flex items-center space-x-3">
+            <Button className="bg-indigo-700 hover:bg-indigo-800">
+              Submit
+            </Button>
+            <Button className="bg-gray-700 hover:bg-gray-800">Cancel</Button>
+          </div>
         </div>
       </Modal>
     </main>
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,6 @@
 import { v4 as uuid } from "uuid";
-import { IProduct } from "../interfaces";
+import { IFormInput, IProduct } from "../interfaces";
+
 export const ProductList: IProduct[] = [
   {
     id: uuid(),
@@ -41,9 +42,111 @@ export const ProductList: IProduct[] = [
     price: "8,100,000 EGP",
     colors: ["Yellow", "red"],
     category: {
-      name: "MBW ",
+      name: "BMW ",
       imageUrl:
         "https://media.hatla2eestatic.com/uploads/car/2024/07/31/6604125/full_up_a16b853d67dc8549506cc2a568ea58dd.jpg",
     },
+  },
+  {
+    id: uuid(),
+    title: "Kia Carens 2014",
+    descriptipn:
+      "Used Kia Carens 2014 for sale Nasr city faster than ever before and more beauty",
+    imageUrl:
+      "https://media.hatla2eestatic.com/uploads/car/2024/07/31/6604511/full_up_c5c8497b6e3be098794c39601d26a658.jpg",
+    price: "710,000 EGP",
+    colors: ["gray", "red"],
+    category: {
+      name: "KIA ",
+      imageUrl:
+        "https://media.hatla2eestatic.com/uploads/car/2024/07/31/6604511/full_up_c5c8497b6e3be098794c39601d26a658.jpg",
+    },
+  },
+  {
+    id: uuid(),
+    title: "HYUNDAI ELANTRA CN7",
+    descriptipn:
+      "HYUNDAI ELANTRA CN7 NEW CASH OR INSTALLMENT Elantra CN7 2024 A/T / BLACK DIAMOND",
+    imageUrl:
+      "https://media.hatla2eestatic.com/uploads/ncarmodel/12293/big-up_8e6ca48eaa051a4dc1821b104a12c89e.png",
+    price: "1,300,000 EGP",
+    colors: ["blue", "red"],
+    category: {
+      name: "HYUNDAI ",
+      imageUrl:
+        "https://media.hatla2eestatic.com/uploads/ncarmodel/12293/big-up_8e6ca48eaa051a4dc1821b104a12c89e.png",
+    },
+  },
+  {
+    id: uuid(),
+    title: "Hyundai Tucson",
+    descriptipn:
+      "Used Hyundai Tucson Turbo GDI 2021 For Sale Sheraton Sell your car faster than ever before",
+    imageUrl:
+      "https://media.hatla2eestatic.com/uploads/car/2024/07/27/6597029/full_up_358171ef9f6f5223d61535e26da1bb18.jpg",
+    price: "1,500,000 EGP",
+    colors: ["black", "gray"],
+    category: {
+      name: "HYUNDAI ",
+      imageUrl:
+        "https://media.hatla2eestatic.com/uploads/ncarmodel/12293/big-up_8e6ca48eaa051a4dc1821b104a12c89e.png",
+    },
+  },
+  {
+    id: uuid(),
+    title: "BMW X5",
+    descriptipn:
+      "Used BMW X5 M 2023 For Sale Tagamo3 - New Cairo Sell your car faster than ever before",
+    imageUrl:
+      "https://media.hatla2eestatic.com/uploads/car/2024/07/31/6604125/full_up_a16b853d67dc8549506cc2a568ea58dd.jpg",
+    price: "8,100,000 EGP",
+    colors: ["Yellow", "red"],
+    category: {
+      name: "BMW ",
+      imageUrl:
+        "https://media.hatla2eestatic.com/uploads/car/2024/07/31/6604125/full_up_a16b853d67dc8549506cc2a568ea58dd.jpg",
+    },
+  },
+  {
+    id: uuid(),
+    title: "Kia Carens 2014",
+    descriptipn:
+      "Used Kia Carens 2014 for sale Nasr city faster than ever before and more beauty",
+    imageUrl:
+      "https://media.hatla2eestatic.com/uploads/car/2024/07/31/6604511/full_up_c5c8497b6e3be098794c39601d26a658.jpg",
+    price: "710,000 EGP",
+    colors: ["gray", "red"],
+    category: {
+      name: "KIA ",
+      imageUrl:
+        "https://media.hatla2eestatic.com/uploads/car/2024/07/31/6604511/full_up_c5c8497b6e3be098794c39601d26a658.jpg",
+    },
+  },
+];
+
+export const formInputList: IFormInput[] = [
+  {
+    id: "title",
+    name: "title",
+    label: "Product Title",
+    type: "text",
+  },
+  {
+    id: "description",
+    name: "description",
+    label: "Description",
+    type: "text",
+  },
+  {
+    id: "image",
+    name: "imageUrl",
+    label: "Product Image",
+    type: "text",
+  },
+  {
+    id: "price",
+    name: "price",
+    label: "Product price",
+    type: "text",
   },
 ];
